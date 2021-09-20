d40e61487807577a25cf6a80258a1eda748e7585
hoanglongne <ngytrhoanglong61@gmail.com>
2021-09-20 17:54:30 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Switch, Link, Route } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from './components';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Homepage</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
